6beec41debc63eee2d9b11ab44fde2cce5803628
composercordero <composercordero@gmail.com>
2023-07-25 23:59:25 -0500

    day 2 homework

---
+++ day2/geometry.py
@@ -0,0 +1,8 @@
+import math
+
+def get_area(radius):
+    return f"The area of a circle with radius {radius} is {3.14*(radius**2)}"
+
+def get_hypotenuse(a,b):
+    return f"the hypotenuse of a right angle with sides of {a}in and {b}in is {math.sqrt(a ** 2 + b ** 2)} in"
+
